50d63afa01b9649191357f972ad52513e8b1cca5
GGB0T11 <gusttrabs@protonmail.com>
2025-11-18 07:43:23 -0300

    itegrei os menus de energia a waybar

--- scripts/scripts/rofi_powermenu.sh
+++ scripts/scripts/rofi_powermenu.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-options=" Lock\n󰠜 Logout\n Shutdown\n Suspend\n Reboot"
+options="Lock \nLogout 󰠜\nShutdown \nSuspend \nReboot "
 
 chosen=$(echo -e "$options" | rofi -dmenu -i -p "Power Menu" -theme ~/.config/rofi/powermenu.rasi)
 
 case "$chosen" in
-" Lock")
-    hyprlock
-    ;;
-" Suspend")
-    systemctl suspend
-    ;;
-" Reboot")
-    systemctl reboot
-    ;;
-" Shutdown")
-    systemctl poweroff
-    ;;
-"󰠜 Logout")
-    hyprctl dispatch exit
-    ;;
+"Lock ")
+  hyprlock
+  ;;
+"Suspend ")
+  systemctl suspend
+  ;;
+"Reboot ")
+  systemctl reboot
+  ;;
+"Shutdown ")
+  systemctl poweroff
+  ;;
+"Logout 󰠜")
+  hyprctl dispatch exit
+  ;;
 esac
--- scripts/scripts/rofi_powerprofiles.sh
+++ scripts/scripts/rofi_powerprofiles.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-options="󰠠 Performance\n󰚀 Balanced\n󰌪 Power-Saver"
+options="Performance 󰠠\nBalanced 󰚀\nPower-Saver 󰌪"
 
 current=$(powerprofilesctl get)
 
 chosen=$(echo -e "$options" | rofi -dmenu -i -p "Power Profiles" -select "$current" -theme ~/.config/rofi/powerprofiles.rasi)
 
 case "$chosen" in
-"󰠠 Performance")
-    powerprofilesctl set performance
-    ;;
-"󰚀 Balanced")
-    powerprofilesctl set balanced
-    ;;
-"󰌪 Power-Saver")
-    powerprofilesctl set power-saver
-    ;;
+"Performance 󰠠")
+  powerprofilesctl set performance
+  ;;
+"Balanced 󰚀")
+  powerprofilesctl set balanced
+  ;;
+"Power-Saver 󰌪")
+  powerprofilesctl set power-saver
+  ;;
 esac
