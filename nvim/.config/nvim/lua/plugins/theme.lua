c481dd9c67f0c6abeff27b73e0f7768695d0e844
GFreitasLab <gusttrabs@protonmail.com>
2025-12-03 17:05:07 -0300

    vou deixar simples, sem firula

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,12 +1,8 @@
 return {
-  "RedsXDD/neopywal.nvim",
-  name = "neopywal",
+  "vague2k/vague.nvim",
   lazy = false,
   priority = 1000,
-  opts = {
-    use_palette = "wallust",
-  },
   config = function()
-    vim.cmd("colorscheme neopywal")
+    vim.cmd("colorscheme vague")
   end,
 }
--- scripts/scripts/wallpaper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-wallpaper=$1
-
-matugen image $wallpaper
-# wallust run "$wallpaper"
-# pkill -SIGUSR1 kitty
-ln -sf "$wallpaper" ~/.config/hypr/current_wallpaper
