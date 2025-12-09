76dae0613995a5221a65ca0d95dd7085d2320901
GFreitasLab <gusttrabs@protonmail.com>
2025-12-09 13:03:33 -0300

    removi o nix-flatpak

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -3,20 +3,18 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    nix-flatpak.url = "github:gmodena/nix-flatpak";
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs"; 
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, nix-flatpak, ... }: {
+  outputs = { self, nixpkgs, home-manager, ... }: {
     nixosConfigurations.nix-machine = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
-        nix-flatpak.nixosModules.nix-flatpak
-	home-manager.nixosModules.home-manager
+	      home-manager.nixosModules.home-manager
       ];
     };
   };
